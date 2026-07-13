b280ab3a8b1afe48dad9856e0ddd9044e13954b3
Matus Ivanecky <matus.ivanecky@gmail.com>
2026-07-13 20:36:42 +0200

    Fix problemov s pdf

--- assets/js/decap-pdf-component.js
+++ assets/js/decap-pdf-component.js
@@ -4,14 +4,15 @@ CMS.registerEditorComponent({
   fields: [
     {name: "id", label: "Google Drive ID", widget: "string"}
   ],
-  pattern: /^{% pdf "(\S+)" %}$/,
+  pattern: /^{% include pdf.html id="(\S+)" type="(\S+)" %}$/,
   fromBlock: function(match) {
     return { id: match[1] };
   },
   toBlock: function(obj) {
-    return `{% pdf "${obj.id}" %}`;
+    // Toto vygeneruje: {% include pdf.html id="1bA0..." type="embed" %}
+    return `{% include pdf.html id="${obj.id}" type="${obj.type}" %}`;
   },
-  toPreview: function(obj) {
+  Preview: function(obj) {
     return `<iframe src="https://drive.google.com/file/d/${obj.id}/preview" width="100%" height="300px"></iframe>`;
   },
 });
